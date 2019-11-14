e51caa711fc478ce953968a3f6aa7cbeacc8bc12
shashank-vats <shashank.vats8@gmail.com>
2019-11-14 14:54:14 +0530

    Finally updated some files

--- Period.h
+++ Period.h
@@ -1,13 +1,20 @@
 #include <bits/stdc++.h>
+#include "Teacher.h"
+
+using namespace std;
+
+struct duration{
+    int hour;
+    int minutes;
+};
+
 class Period{
     
-    struct duration{
-        int hour;
-        int minutes;
-    };
-
-    string subject="";
-    string Class="";
+    private:
+    string subject;
+    string Class;
     Teacher teacher;
 
+    public:
+    Period(string sub, string sec_name, string teacher_name)
 };
