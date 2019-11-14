4a7a390d0f0eae15eff99cb5b0dd8e123c710df0
shashank-vats <shashank.vats8@gmail.com>
2019-11-14 14:37:08 +0530

    Updated some files

---
+++ Section.h
@@ -0,0 +1,19 @@
+#ifndef SECTION_H
+#define SECTION_H
+
+#include <bits/stdc++.h>
+#include "Period.h"
+
+using namespace std;
+
+class Section {
+    private:
+    int numPeriods;
+    int numSubjects;
+    string name;
+    vector<Period> timeTable;
+
+    public:
+}
+
+#endif
