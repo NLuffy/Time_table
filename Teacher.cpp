68014ffea050d6f7fa3354e01fe7b2ef4972868a
shashank-vats <shashank.vats8@gmail.com>
2019-11-18 14:59:28 +0530

    Made some changes

--- Teacher.cpp
+++ Teacher.cpp
@@ -5,7 +5,7 @@ Teacher::Teacher(string name, int maxPeriodsPerDay){
     this->name=name;
     this->maxPeriodsPerDay= maxPeriodsPerDay;
 
-    this->teacher_time_table=vector<vector<Period>> (global.WeekDays, vector<Period>(global.periodsPerDay));
+    this->teacher_time_table=vector<vector<Period>> (GlobalVariables.WeekDays, vector<Period>(GlobalVariables.periodsPerDay));
 }
 void Teacher::setQualification(string sub, set<int>&qual){
     this->qualifications[sub]=qual;
